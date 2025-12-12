a4a510cab27a0334b31e91be1b6b308869a00848
PooravK <pooravkumar0908@gmail.com>
2025-12-12 19:54:06 +0530

    Practice

--- C++/ProblemSet_90/7EvenOddPolarity.cpp
+++ C++/ProblemSet_90/7EvenOddPolarity.cpp
@@ -3,5 +3,36 @@ using namespace std;
 
 int main()
 {
+    int in0;
+    cout << "Enter a number: ";
+    cin >> in0;
+
+    if (in0 > 0)
+    {
+        cout << "Positive" << endl;
+    }
+    if (in0 < 0)
+    {
+        cout << "Negative" << endl;
+    }
+    if (in0 == 0)
+    {
+        cout << "Zero" << endl;
+    }
+    if (in0 % 2 == 0)
+    {
+        if (in0 == 0)
+        {
+
+        }
+        else
+        {
+            cout << "Even" << endl;
+        }
+    }
+    else 
+    {
+        cout << "Odd" << endl;
+    }
     return 0;
 }
