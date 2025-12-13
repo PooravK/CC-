849383a7fd8257b9c1b7f26b09eaa83c6a4eeb64
PooravK <boomster.work@gmail.com>
2025-12-13 23:01:06 +0530

    Practice

--- C++/ProblemSet_90/11DigitOperations.cpp
+++ C++/ProblemSet_90/11DigitOperations.cpp
@@ -3,5 +3,25 @@ using namespace std;
 
 int main ()
 {
+    int n;
+    cout << "Enter an integer: ";
+    cin >> n;
+
+    int count = 0;
+    int immediate = n;
+    int sum = 0;
+    int new_num = 0;
+
+    while (immediate != 0)
+    {
+        sum = sum + (immediate % 10);
+        new_num = (immediate % 10) + (new_num * 10);
+        immediate = immediate/10;
+        count++;
+    }
+
+    cout << count << endl;
+    cout << sum << endl;
+    cout << new_num << endl;
     return 0;
 }
